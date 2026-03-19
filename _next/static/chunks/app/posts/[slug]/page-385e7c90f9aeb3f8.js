00c66b0c929acf2960528c4dd394d705e9656cee
KiCola <KiCola@users.noreply.github.com>
2026-03-19 08:52:09 +0000

    deploy: 1f7000f2985059b2426c7903444416a05de57e32

---
+++ _next/static/chunks/app/posts/[slug]/page-385e7c90f9aeb3f8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[333],{9803:function(n,e,t){Promise.resolve().then(t.t.bind(t,2972,23)),Promise.resolve().then(t.t.bind(t,3320,23))},3320:function(){}},function(n){n.O(0,[166,972,971,117,744],function(){return n(n.s=9803)}),_N_E=n.O()}]);
